1822864f4c98e4beeed1c3e430012b012f87c325
moin04-newbie <moinsayyad529@gmail.com>
2026-02-02 13:15:19 +0530

    New Project add

--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -2,28 +2,49 @@ import adidasSphereImage from '../assets/images/Adidas Sphere.png';
 import supplySnapImage from '../assets/images/Supply snap.png';
 import smartEDImage from '../assets/images/SmartED.png';
 import nextWaveImage from '../assets/images/x.png';
+import squadeImage from '../assets/images/Squade.png';
+import agencyWebsiteImage from '../assets/images/ScaleX.png';
+import aiDocumentConverterImage from '../assets/images/Weave.png';
 
 const projects = [
   {
-    id: 1,
+    id: 0,
     name: "Adidas Sphere",
     image: adidasSphereImage,
     link: "https://adidas-iota.vercel.app/"
   },
   {
+    id: 1,
+    name: "Ai Assistant",
+    image: squadeImage,
+    link: "https://stillworking-rouge.vercel.app/"
+  },
+  {
     id: 2,
+    name: "Agency Website",
+    image: agencyWebsiteImage,
+    link: "https://studio-two-sepia.vercel.app/"
+  },
+  {
+    id: 3,
+    name: "AI Document Converter",
+    image: aiDocumentConverterImage,
+    link: "https://frontend-silk-ten-47.vercel.app/"
+  },
+  {
+    id: 4,
     name: "Supply Snap",
     image: supplySnapImage,
     link: "https://supplysnap.vercel.app/"
   },
   {
-    id: 3,
+    id: 5,
     name: "SmartED",
     image: smartEDImage,
     link: "https://v0-smart-ed-platform.vercel.app/"
   },
   {
-    id: 4,
+    id: 6,
     name: "NextWave",
     image: nextWaveImage,
     link: "https://raman-s-future.vercel.app/"
